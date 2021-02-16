61614bcd532e26cc3e14686fd3e8735a9169b4a1
The-Renaissance <nashesvobodnoye@hotmail.com>
2021-02-15 21:33:19 -0500

    Day 10 Part 1

---
+++ 2020/Day 10/Puzzle.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// FindNumberofDifferences returns the number of 1-jolt and 3-jolt differences
+func FindNumberofDifferences(adapters []int) (onejolts, threejolts int) {
+	sort.Ints(adapters)
+	for i, rating := range adapters {
+		var diff int
+		if i == 0 {
+			diff = rating - 0
+		} else {
+			diff = rating - adapters[i-1]
+		}
+		if diff == 1 {
+			onejolts++
+		} else if diff == 3 {
+			threejolts++
+		}
+	}
+	threejolts++ // difference between largest adapter and target is always 3
+	return
+}
+
+func parseList(filename string) []int {
+	lines := getInput(filename)
+	list := make([]int, len(lines))
+	for i, line := range lines {
+		if num, err := strconv.Atoi(line); err != nil {
+			log.Panicf("parsing %v at line %v failed\n", line, i+1)
+		} else {
+			list = append(list, num)
+		}
+	}
+	return list
+}
+
+func getInput(filename string) []string {
+	l, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("Reading", filename, "failed:", err)
+	}
+	return strings.Split(strings.Trim(string(l), "\n"), "\n")
+}
+
+func solvePart1() {
+	adapters := parseList("input.txt")
+	onejolts, threejolts := FindNumberofDifferences(adapters)
+	fmt.Printf("Part 1: %v\n", onejolts*threejolts)
+}
+
+func main() {
+	solvePart1()
+}
---
+++ 2020/Day 10/Puzzle_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestFindNumberofDifferences(t *testing.T) {
+	type result struct {
+		onejolts, threejolts int
+	}
+	cases := []struct {
+		name     string
+		adapters []int
+		want     result
+	}{
+		{
+			"Example",
+			[]int{16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4},
+			result{7, 5},
+		},
+		{
+			"Larger Example",
+			[]int{28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3},
+			result{22, 10},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			if onejolts, threejolts := FindNumberofDifferences(tt.adapters); onejolts != tt.want.onejolts || threejolts != tt.want.threejolts {
+				t.Errorf("FindNumberofDifferences() = %v,%v want %v,%v", onejolts, threejolts, tt.want.onejolts, tt.want.threejolts)
+			}
+		})
+	}
+}
