2aa0de098896ea4df06adf74c57ac9f365c5e824
The-Renaissance <nashesvobodnoye@hotmail.com>
2021-02-16 20:37:01 -0500

    Day 11 Part 1

---
+++ 2020/Day 11/Puzzle.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"reflect"
+	"strings"
+)
+
+// SeatMap stores seat state information
+type SeatMap [][]rune
+
+// PPrint pretty prints a SeatMap
+func (m SeatMap) PPrint() string {
+	rows := make([]string, len(m))
+	for i := range rows {
+		rows[i] = string(m[i])
+	}
+	return strings.Join(rows, "\n")
+}
+
+// FloorState stores the previous and next states of a seat floor
+type FloorState struct {
+	previous SeatMap
+	current  SeatMap
+}
+
+// Position encodes a seat's row and column in the map
+type Position struct {
+	row, column int
+}
+
+// AdvanceState runs one step of the simulation
+func (s *FloorState) AdvanceState() {
+	if s.previous == nil {
+		s.previous = make([][]rune, len(s.current))
+	}
+	for i := range s.previous {
+		s.previous[i] = make([]rune, len(s.current[0]))
+		copy(s.previous[i], s.current[i])
+	}
+	for i, row := range s.previous {
+		for j, seat := range row {
+			adjacents := s.getadjacents(Position{i, j})
+			if seat == 'L' {
+				noOccupied := true
+				for _, adjseat := range adjacents {
+					if s.previous[adjseat.row][adjseat.column] == '#' {
+						noOccupied = false
+						break
+					}
+				}
+				if noOccupied {
+					s.current[i][j] = '#'
+				}
+			} else if seat == '#' {
+				occupiedSeatCount := 0
+				for _, adjseat := range adjacents {
+					if s.previous[adjseat.row][adjseat.column] == '#' {
+						occupiedSeatCount++
+					}
+				}
+				if occupiedSeatCount >= 4 {
+					s.current[i][j] = 'L'
+				}
+			}
+		}
+	}
+}
+
+// RunandCount runs the simulation up until previous == current then counts occupied seats
+func (s *FloorState) RunandCount() int {
+	for !reflect.DeepEqual(s.previous, s.current) {
+		s.AdvanceState()
+	}
+	occupiedSeatCount := 0
+	for _, row := range s.current {
+		for _, seat := range row {
+			if seat == '#' {
+				occupiedSeatCount++
+			}
+		}
+	}
+	return occupiedSeatCount
+}
+
+func (s *FloorState) getadjacents(p Position) []Position {
+	rows, columns := len(s.current), len(s.current[0])
+	i, j := p.row, p.column
+	var pos []Position
+	if i > 0 && j > 0 {
+		pos = append(pos, Position{i - 1, j - 1})
+	}
+	if i > 0 {
+		pos = append(pos, Position{i - 1, j})
+	}
+	if i > 0 && j+1 < columns {
+		pos = append(pos, Position{i - 1, j + 1})
+	}
+	if j > 0 {
+		pos = append(pos, Position{i, j - 1})
+	}
+	if j+1 < columns {
+		pos = append(pos, Position{i, j + 1})
+	}
+	if i+1 < rows && j > 0 {
+		pos = append(pos, Position{i + 1, j - 1})
+	}
+	if i+1 < rows {
+		pos = append(pos, Position{i + 1, j})
+	}
+	if i+1 < rows && j+1 < columns {
+		pos = append(pos, Position{i + 1, j + 1})
+	}
+	return pos
+}
+
+// FromInitialState returns a FloorState struct from an initial state
+func FromInitialState(init []string) *FloorState {
+	fs := FloorState{
+		current: make([][]rune, 0, len(init)),
+	}
+	for _, row := range init {
+		fs.current = append(fs.current, []rune(row))
+	}
+	return &fs
+}
+
+func getInput(filename string) []string {
+	l, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalln("Reading", filename, "failed:", err)
+	}
+	return strings.Split(strings.Trim(string(l), "\n"), "\n")
+}
+
+func solvePart1() {
+	init := getInput("input.txt")
+	fs := FromInitialState(init)
+	fmt.Printf("Part 1: %v\n", fs.RunandCount())
+}
+
+func main() {
+	solvePart1()
+}
---
+++ 2020/Day 11/Puzzle_test.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFloorState_AdvanceState(t *testing.T) {
+	tests := []struct {
+		name string
+		s    *FloorState
+		next SeatMap
+	}{
+		{
+			"Case 1",
+			&FloorState{
+				current: SeatMap{
+					[]rune("L.LL.LL.LL"),
+					[]rune("LLLLLLL.LL"),
+					[]rune("L.L.L..L.."),
+					[]rune("LLLL.LL.LL"),
+					[]rune("L.LL.LL.LL"),
+					[]rune("L.LLLLL.LL"),
+					[]rune("..L.L....."),
+					[]rune("LLLLLLLLLL"),
+					[]rune("L.LLLLLL.L"),
+					[]rune("L.LLLLL.LL"),
+				},
+			},
+			SeatMap{
+				[]rune("#.##.##.##"),
+				[]rune("#######.##"),
+				[]rune("#.#.#..#.."),
+				[]rune("####.##.##"),
+				[]rune("#.##.##.##"),
+				[]rune("#.#####.##"),
+				[]rune("..#.#....."),
+				[]rune("##########"),
+				[]rune("#.######.#"),
+				[]rune("#.#####.##"),
+			},
+		},
+		{
+			"Case 2",
+			&FloorState{
+				current: SeatMap{
+					[]rune("#.#L.L#.##"),
+					[]rune("#LLL#LL.L#"),
+					[]rune("L.L.L..#.."),
+					[]rune("#LLL.##.L#"),
+					[]rune("#.LL.LL.LL"),
+					[]rune("#.LL#L#.##"),
+					[]rune("..L.L....."),
+					[]rune("#L#LLLL#L#"),
+					[]rune("#.LLLLLL.L"),
+					[]rune("#.#L#L#.##"),
+				},
+			},
+			SeatMap{
+				[]rune("#.#L.L#.##"),
+				[]rune("#LLL#LL.L#"),
+				[]rune("L.#.L..#.."),
+				[]rune("#L##.##.L#"),
+				[]rune("#.#L.LL.LL"),
+				[]rune("#.#L#L#.##"),
+				[]rune("..L.L....."),
+				[]rune("#L#L##L#L#"),
+				[]rune("#.LLLLLL.L"),
+				[]rune("#.#L#L#.##"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Logf("Testing state: \n%s", tt.s.current.PPrint())
+			tt.s.AdvanceState()
+			if !reflect.DeepEqual(tt.s.current, tt.next) {
+				t.Errorf("Want: \n%s\n\n, got \n\n%s", tt.next.PPrint(), tt.s.current.PPrint())
+			}
+		})
+	}
+}
+
+func TestFloorState_RunandCount(t *testing.T) {
+	tests := []struct {
+		name string
+		s    *FloorState
+		want int
+	}{
+		{
+			"Example",
+			FromInitialState(
+				[]string{
+					"L.LL.LL.LL",
+					"LLLLLLL.LL",
+					"L.L.L..L..",
+					"LLLL.LL.LL",
+					"L.LL.LL.LL",
+					"L.LLLLL.LL",
+					"..L.L.....",
+					"LLLLLLLLLL",
+					"L.LLLLLL.L",
+					"L.LLLLL.LL",
+				},
+			),
+			37,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.s.RunandCount(); got != tt.want {
+				t.Errorf("FloorState.RunandCount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
